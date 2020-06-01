317b0fabe504189923001eee08ba8fbe38bfa1f9
yoji yamaura <mirrorball.pet.rolzer@gmail.com>
2020-06-01 09:21:35 +0900

    wip version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Peer from 'skyway-js';
 import VideoComp from './components/videoComp';
-
-type Stream = {
-  localStream: MediaStream;
-}
-
-const peer = new Peer({
-  key: 'acbe9e88-dfd6-495a-8ab3-8aae221fe08e',
-  debug: 3
-});
 
 function App() {
 
   return (
-
     <div className="App">
       video
-      <VideoComp peer={peer}/>
+      <VideoComp />
     </div>
   );
 }
--- src/components/videoComp.tsx
+++ src/components/videoComp.tsx
@@ -1,80 +1,56 @@
-import React, {Component, useRef, useEffect, useState} from 'react';
+import React, {Component, useRef, useState} from 'react';
 import '../App.css';
 import Peer from 'skyway-js';
 
 type Props = {
-  peer: Peer
 };
 
-const VideoComp: React.FC<Props> = ( {peer} ) => { 
+const peer = new Peer({
+  key: '',
+  debug: 3
+});
+
+const VideoComp: React.FC<Props> = ( {} ) => {
+  const [myId, setMyId] = useState('');
+  const [callId, setCallId] = useState('')
+
   // useRefは明示的にvideoElmentを指定する
   const videoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
 
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-
-  const theirVideoRef = useRef<HTMLVideoElement>(null);
-
-  const [myId, setMyId] = useState('');
-
-  //const [localStream, setLoacalStream] = useState<MediaStream | undefined>(undefined);
   let localStream:(MediaStream | undefined) = undefined;
 
-  // useEffectで後から処理になるはず
-  useEffect(() => {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
-        .then( stream => {
-        // 成功時にvideo要素にカメラ映像をセットし、再生
-        if (!videoRef.current) return
-        videoRef.current.srcObject = stream;
-        videoRef.current.play();
-        // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
-        //localStream = stream;
-        console.log("localStreamTmp is ++++++++++++");
-        localStream = stream;
-        console.log(localStream);
-        
-    }).catch( error => {
-        // 失敗時にはエラーログを出力
-        console.error('mediaDevice.getUserMedia() error:', error);
-        return;
-    });
+  navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(strem => {
+    if (!videoRef.current) return; 
+    videoRef.current.srcObject = strem;
+    localStream = strem;
   });
 
+  // PeerID 発番
   peer.on('open', () => {
     setMyId(peer.id);
   });
-
+  
   // 発信処理
   const handleClick = () => {
-    let theirID = textAreaRef.current?.value;
-    const mediaConnection = peer.call(theirID!, localStream);
+    const mediaConnection = peer.call(callId, localStream!);
     
-    mediaConnection.on('stream', stream => {
+    mediaConnection.on('stream', async stream => {
       // video要素にカメラ映像をセットして再生
-      if (!theirVideoRef.current) return
-      theirVideoRef.current.srcObject = stream;
-      theirVideoRef.current.play();
+      if (!remoteVideoRef.current) return
+      remoteVideoRef.current.srcObject = stream;
+      await remoteVideoRef.current.play();
     });
   }
 
   // 受信処理
   peer.on('call', mediaConnection => {
-    console.log("受信処理1111111111111");
     mediaConnection.answer(localStream);
-    console.log("受信処理2222222222222");
-    mediaConnection.on('stream', stream => {
-      // video要素にカメラ映像をセットして再生
-      if (!theirVideoRef.current) return
-      console.log("受信処理333333333333333");
-      theirVideoRef.current.srcObject = stream;
-      console.log("受信処理4444444444444444");
-      theirVideoRef.current.play();
-    });
-  });
 
-  // エラー処理
-  peer.on('error', err => {
-    alert(err.message);
+    mediaConnection.on('stream', async stream => {
+      if (!remoteVideoRef.current) return
+      remoteVideoRef.current.srcObject = stream;
+    });
   });
 
   return (
@@ -85,12 +61,12 @@ const VideoComp: React.FC<Props> = ( {peer} ) => {
         <video id="my-video" width="400px" ref={videoRef} autoPlay muted playsInline></video>
       </div>
       <div>
-        <textarea id="their-id" ref={textAreaRef}></textarea>
+      <input value={callId} onChange={e => setCallId(e.target.value)}></input>
         <button id="make-call" onClick={handleClick}>発信</button>
       </div>
       <div>
         Their Video
-        <video id="theire-video" width="400px" ref={theirVideoRef} autoPlay muted playsInline></video>
+        <video id="remote-video" width="400px" ref={remoteVideoRef} autoPlay muted playsInline></video>
       </div>
     </div>
   );
