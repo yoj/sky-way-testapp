4411fb5d363c6847509762c5ab01be10c9a18b6f
山浦 洋司 <yoji_yamaura@mdmb01.local>
2020-06-10 14:45:40 +0900

    MaterialUI 追加 RoomId画面に適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import VideoComp from './components/VideoComp';
 import RoomComp from './components/RoomComp';
